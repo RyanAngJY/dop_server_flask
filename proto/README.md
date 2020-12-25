ae4ab81fe9a1a3a383cbfa6d5f94402d8f5a72ef
RyanAngJY <ryanang.jy@gmail.com>
2020-12-25 19:49:55 +0800

    Add script to download proto

---
+++ proto/README.md
@@ -0,0 +1,1 @@
+Run ./install_proto_common.sh to download protofiles from the dop_proto_common repository
---
+++ proto/install_proto_common.sh
@@ -0,0 +1,28 @@
+download_dep() {
+    local proto_dep=$1
+    folder=${proto_dep//.//} # replace "." with "/"
+    filename=$(echo $proto_dep | sed 's/.*\.//') # find the filename (the substring after the last ".")
+    echo $folder
+    echo $filename
+
+    dest_file=./dep/proto/$folder/$filename.proto
+    github_link=https://raw.githubusercontent.com/RyanAngJY/dop_proto_common/master/$folder/$filename.proto
+    
+    # Pull file from github:
+    curl -o $dest_file -LJ $github_link --create-dirs # create parent dirs locally if don't exist
+}
+
+get_config() {
+    # Get config from ./dep_proto_common.json file
+    # jq is a brew installed library that helps with reading JSON in bash
+    # the tr command converts JSON array to bash array by removing angular brackets and commas
+    echo $(jq -r '.dep' ./dep_proto_common.json | tr -d '[]," ') 
+}
+
+brew install jq
+
+# Download all deps specified
+proto_deps=$(get_config)
+for proto_dep in ${proto_deps[@]}; do
+    download_dep ${proto_dep}
+done
